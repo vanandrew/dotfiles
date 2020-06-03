f45103afa1e4fb3d03e8c885a7ae8ca5f23a086e
Andrew Van <vanandrew@wustl.edu>
2020-06-03 14:38:34 -0500

    update uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 # Uninstall script
-[ -f ~/.zsh* ] && rm ~/.zsh*
-[ -f ~/.zcompdump* ] && rm ~/.zcompdump*
-[ -d ~/.oh-my-zsh ] && rm -rf ~/.oh-my-zsh
-[ -f ~/.p10k.zsh ] && rm ~/.p10k.zsh
-[ -f ~/.login ] && rm ~/.login
-[ -f ~/.login.bak ] && mv ~/.login.bak ~/.login
-[ -f ~/.bash_profile ] && rm ~/.bash_profile
-[ -f ~/.bash_profile.bak ] && mv ~/.bash_profile.bak ~/.bash_profile
-[ -d ~/.dotfiles ] && rm -rf ~/.dotfiles
+[[ -f ~/.zshrc ]] && rm ~/.zshrc
+[[ -f ~/.zsh_history ]] && rm ~/.zsh_history
+[[ -f ~/.zcompdump* ]] && rm ~/.zcompdump*
+[[ -d ~/.oh-my-zsh ]] && rm -rf ~/.oh-my-zsh
+[[ -f ~/.p10k.zsh ]] && rm ~/.p10k.zsh
+[[ -f ~/.login ]] && rm ~/.login
+[[ -f ~/.login.bak ]] && mv ~/.login.bak ~/.login
+[[ -f ~/.bash_profile ]] && rm ~/.bash_profile
+[[ -f ~/.bash_profile.bak ]] && mv ~/.bash_profile.bak ~/.bash_profile
+[[ -d ~/.dotfiles ]] && rm -rf ~/.dotfiles
 echo "Uninstall Complete!"
