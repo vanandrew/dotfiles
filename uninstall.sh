42d2acf0f8231c7cb693ab60835b9847ffbe5564
Andrew Van <vanandrew@wustl.edu>
2020-07-20 11:20:13 -0500

    Dont delete essential files on uninstall; only overwrite when backup exists

--- uninstall.sh
+++ uninstall.sh
@@ -4,13 +4,9 @@
 [[ -f ~/.zsh_history ]] && rm ~/.zsh_history
 [[ -d ~/.oh-my-zsh ]] && rm -rf ~/.oh-my-zsh
 [[ -f ~/.p10k.zsh ]] && rm ~/.p10k.zsh
-[[ -f ~/.login ]] && rm ~/.login
 [[ -f ~/.login.bak ]] && mv ~/.login.bak ~/.login
-[[ -f ~/.bash_profile ]] && rm ~/.bash_profile
 [[ -f ~/.bash_profile.bak ]] && mv ~/.bash_profile.bak ~/.bash_profile
-[[ -f ~/.cshrc ]] && rm ~/.cshrc
 [[ -f ~/.cshrc.bak ]] && mv ~/.cshrc.bak ~/.cshrc
-[[ -f ~/.bashrc ]] && rm ~/.bashrc
 [[ -f ~/.bashrc.bak ]] && mv ~/.bashrc.bak ~/.bashrc
 [[ -d ~/.dotfiles ]] && rm -rf ~/.dotfiles
 echo "Uninstall Complete!"
