599976e0451e46f1fc9b063829cc7203901c1481
Andrew Van <vanandrew@wustl.edu>
2020-11-24 09:38:55 +0000

    hide pushd/popd output

--- install.sh
+++ install.sh
@@ -166,25 +166,25 @@ fi
 
 # change to home directory
 echo "Adding in zsh plugins..."
-pushd ~
+pushd ~ > /dev/null
 # git clone the lab dotfile repo if not exist; otherwise just pull
 if [ -d ~/.dotfiles ]; then
-  pushd ~/.dotfiles
+  pushd ~/.dotfiles > /dev/null
   git pull
-  popd
+  popd > /dev/null
 else
   git clone https://gitlab.com/DosenbachGreene/dotfiles.git ~/.dotfiles
 fi
 # clone oh-my-zsh
 [ ! -d ~/.oh-my-zsh ] && git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 # clone theme
-pushd ~/.oh-my-zsh/custom/themes
+pushd ~/.oh-my-zsh/custom/themes > /dev/null
 [ ! -d ./powerlevel10k ] && git clone https://github.com/romkatv/powerlevel10k.git
 cd powerlevel10k
 git fetch && git checkout v1.13.0
-popd
+popd > /dev/null
 # clone plugins
-pushd ~/.oh-my-zsh/custom/plugins
+pushd ~/.oh-my-zsh/custom/plugins > /dev/null
 [ ! -d zsh-autosuggestions ] && git clone https://github.com/zsh-users/zsh-autosuggestions.git
 cd zsh-autosuggestions
 git fetch && git checkout v0.6.4
@@ -193,12 +193,12 @@ cd ..
 cd zsh-syntax-highlighting
 git fetch && git checkout 0.7.1
 cd ..
-popd
+popd > /dev/null
 # the current zshrc in home if not exist
 [ ! -f ~/.zshrc ] && cp ~/.dotfiles/zshrc ~/.zshrc
 # add paths_user file if not exist
 [ ! -f ~/.paths_user ] && cp ~/.dotfiles/paths_user ~/.paths_user
-popd
+popd > /dev/null
 
 echo ""
 echo "######################################################"
