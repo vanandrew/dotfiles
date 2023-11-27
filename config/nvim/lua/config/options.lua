72eeedb40a8d6fa29f6977ed979ed4717c40378c
Andrew Van <vanandrew77@gmail.com>
2023-11-27 15:45:57 -0600

    :sparkles: add cursor wrap to neovim

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,3 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.wo.rnu = false
+vim.opt.whichwrap:append({
+  ["<"] = true,
+  [">"] = true,
+  ["["] = true,
+  ["]"] = true,
+  h = true,
+  l = true,
+})
